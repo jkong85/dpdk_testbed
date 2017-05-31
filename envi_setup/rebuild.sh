cd82b1f592b20c2cbd75a7cdd04d0b79b59a3174
envi <kongjian.work@gmail.com>
2017-05-30 17:38:26 -0700

    test pmd

--- envi_setup/rebuild.sh
+++ envi_setup/rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -rf ../test-pmd /usr/src/dpdk-stable-16.11.1/app/test-pmd
+cp -rf ../test-pmd /usr/src/dpdk-stable-16.11.1/app/
 
 pushd $DPDK_DIR
 make install T=$DPDK_TARGET DESTDIR=install
